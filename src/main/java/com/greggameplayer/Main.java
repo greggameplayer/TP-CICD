6cac2cbfacf1ea20aed1408f26363a8b36624a13
greggameplayer <gregoire.hage@gmail.com>
2022-11-23 11:55:35 +0100

    first compilable version

--- src/main/java/com/greggameplayer/Main.java
+++ src/main/java/com/greggameplayer/Main.java
@@ -1,4 +1,6 @@
-package com.greggameplayer;public class Main {
+package com.greggameplayer;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
