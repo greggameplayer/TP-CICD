1a87c08df76e13f5e94beb0866928c5a2449a8df
greggameplayer <gregoire.hage@gmail.com>
2022-11-24 14:40:26 +0100

    add unit tests

---
+++ src/test/java/com/greggameplayer/MainTest.java
@@ -0,0 +1,36 @@
+package com.greggameplayer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void stubTest() {
+        Main.main(new String[]{});
+        assertEquals("CI/CD rocks!\r\n", outContent.toString());
+    }
+}
